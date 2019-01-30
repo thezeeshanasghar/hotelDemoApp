e9b66256d088757c186ffd6630c2b8e15d4f16de
MuhammadInaamMunir <muhammad.inaam@aurea.com>
2019-01-30 23:18:17 +0500

    fix navbar, add menu and icons in it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  public appMenu = [
+    {title:'Our Menu', url:'/home',icon:'list'},
+    {title:'Reservation', url:'/home',icon:'add-circle-outline'},
+    {title:'Our Deals', url:'/home',icon:'notifications'},
+    {title:'Locations', url:'/home',icon:'egg'},
+    {title:'My Basket', url:'/home',icon:'basket'},
+    {title:'Feedback', url:'/home',icon:'checkmark-circle-outline'},
+    {title:'Edit Profile', url:'/home',icon:'person-add'}
+  ];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
